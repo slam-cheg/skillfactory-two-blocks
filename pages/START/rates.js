296933c4717eb84d4c41b41df21f495e61c62c8f
SSTE <levashushkov@yandex.ru>
2023-11-27 13:43:23 +0300

    paddings

--- pages/START/rates.js
+++ pages/START/rates.js
@@ -86,3 +86,29 @@ const ratesSlider = new Swiper(".takeit__slider", {
 		},
 	},
 });
+
+function openPopup(currentPopup) {
+	currentPopup.classList.add("popup_opened");
+	currentPopup.addEventListener("click", handleOverlayClick);
+	window.addEventListener("keydown", closeByEscape);
+}
+
+function closePopup(currentPopup) {
+	currentPopup.classList.remove("popup_opened");
+	window.removeEventListener("keydown", closeByEscape);
+}
+
+function handleOverlayClick(event) {
+	if (event.type === "click") {
+		if (event.target === event.currentTarget) {
+			closePopup(event.target);
+		}
+	}
+}
+
+function closeByEscape(event) {
+	if (event.key === "Escape") {
+		const openedPopUp = document.querySelector(".popup_opened");
+		closePopup(openedPopUp);
+	}
+}
