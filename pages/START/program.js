77e0d21107f3d5e3a43230258dd170675d0c968c
SSTE <levashushkov@yandex.ru>
2023-11-27 13:12:49 +0300

    texts

--- pages/START/program.js
+++ pages/START/program.js
@@ -1,6 +1,8 @@
 programAccordItems.forEach((element) => {
-	const programAccordHeader = element.querySelector(".accord-header");
-	programAccordHeader.addEventListener("click", () => {
-		handlerAccordToggle(element);
-	});
+	if (!element.classList.contains("accord-inner__element_notopenable")) {
+		const programAccordHeader = element.querySelector(".accord-header");
+		programAccordHeader.addEventListener("click", () => {
+			handlerAccordToggle(element);
+		});
+	}
 });
