a969a959983ce37a23246f5f4ef8fc4df7accbf3
Lev Shushkov <levashushkov@yandex.ru>
2024-05-17 15:35:58 +0300

    rebuild test-page

--- pages/Banners/SF/07.05.2024 (110)/index.js
+++ pages/Banners/SF/07.05.2024 (110)/index.js
@@ -2,11 +2,16 @@ const title = document.querySelector(".title");
 const may = document.querySelector(".may");
 const subtitle = document.querySelector(".subtitle");
 
-setTimeout(()=>{anim(may)}, 400);
-setTimeout(() => {anim(title)}, 900);
-setTimeout(()=>{anim(subtitle)}, 1200);
-
+setTimeout(() => {
+	anim(may);
+}, 400);
+setTimeout(() => {
+	anim(title);
+}, 900);
+setTimeout(() => {
+	anim(subtitle);
+}, 1200);
 
 function anim(elem) {
-    elem.classList.remove("anim")
+	elem.classList.remove("anim");
 }
