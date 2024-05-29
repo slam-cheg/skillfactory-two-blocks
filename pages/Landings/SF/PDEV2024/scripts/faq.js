e38ea2d9f847befe53f0727363a3f1390fa1d6f4
Lev Shushkov <levashushkov@yandex.ru>
2024-05-29 10:33:02 +0300

    changes

--- pages/Landings/SF/PDEV2024/scripts/faq.js
+++ pages/Landings/SF/PDEV2024/scripts/faq.js
@@ -1,5 +1,5 @@
 import InitAccordeon from "./accordeon.js";
-const faqBlock = document.querySelector(".faq");
 
+const faqBlock = document.querySelector(".faq");
 const faqAccordeon = new InitAccordeon(faqBlock);
 faqAccordeon.init();
