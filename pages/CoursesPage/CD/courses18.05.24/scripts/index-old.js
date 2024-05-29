39b1929a5e893e4167fb6a146b6113f0c17aa047
Lev Shushkov <levashushkov@yandex.ru>
2024-05-29 12:54:20 +0300

    font NT Somic

--- pages/CoursesPage/CD/courses18.05.24/scripts/index-old.js
+++ pages/CoursesPage/CD/courses18.05.24/scripts/index-old.js
@@ -6,17 +6,32 @@ const hiddenCards = courses.querySelectorAll(".card_hidden");
 hideButton.addEventListener("click", hideUnhideCards);
 
 function hideUnhideCards() {
+	let timeout = 0;
+
 	if (hideButton.dataset.hide === "hidden") {
 		for (let i = 0; i < hiddenCards.length; i++) {
-			hiddenCards[i].classList.remove("card_hidden");
+			setTimeout(() => {
+				hiddenCards[i].style.display = null;
+				setTimeout(() => {
+					hiddenCards[i].classList.remove("card_hidden");
+				}, 100);
+			}, timeout);
+			timeout += 100;
 		}
 		hideButton.dataset.hide = "visible";
 		return;
 	}
 
 	if (hideButton.dataset.hide === "visible") {
+
 		for (let i = hiddenCards.length - 1; i >= 0; i--) {
-			hiddenCards[i].classList.add("card_hidden");
+			setTimeout(() => {
+				hiddenCards[i].classList.add("card_hidden");
+				setTimeout(() => {
+					hiddenCards[i].style.display = "none";
+				}, 500);
+			}, timeout);
+			timeout += 100;
 		}
 
 		hideButton.dataset.hide = "hidden";
